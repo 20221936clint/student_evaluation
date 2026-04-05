e22c74daaa8a48262aba6db7b5c7a66a6899ea63
justinquidit12 <justinquidit56@gmail.com>
2026-04-06 01:23:22 +0800

    favicon

--- Door/admin/dashboard.php
+++ Door/admin/dashboard.php
@@ -17,6 +17,7 @@ if (!file_exists($page_file)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../../media/LOGO.jpg" type="image/jpeg">
     <title>Admin Dashboard - Faculty Evaluation System</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
